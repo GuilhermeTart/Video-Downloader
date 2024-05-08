1582fbe6c8bf35d5787836d123805adfae24305b
GuilhermeTart <136984328+GuilhermeTart@users.noreply.github.com>
2024-05-08 20:25:20 -0300

    Update Video_Downloader.py

--- Video_Downloader.py
+++ Video_Downloader.py
@@ -1,76 +1,77 @@
-import subprocess
 import os
+import subprocess
 #
 from colors import colors
 from sys import exit
 
 
 
+while True:
+        
+    def download_movie(movie_url, path):
+        path = 'Videos_Download' 
+        command = ["yt-dlp", "-f", "mp4", movie_url, "-o", f'{path}/%(title)s.%(ext)s']
+        output = subprocess.check_output(command) 
+        try:
+            if not os.path.exists(path):
+                os.mkdir(path)
+                print('Pasta Criada!')
+            else:
+                print(colors.red +'Pasta já existe'+ colors.reset)
+            #
+        except OSError:
+            print("Não foi possível encontrar o caminho! Verifique!!")
+            raise
+        except Exception:
+            raise
+        # 
+        print(colors.blue + output.decode('latin-1') + colors.reset)
     
-def download_movie(movie_url, path):
-    path = 'Videos_Download' 
-    command = ["yt-dlp", "-f", "mp4", movie_url, "-o", f'{path}/%(title)s.%(ext)s']
-    output = subprocess.check_output(command) 
-    try:
-        if not os.path.exists(path):
-            os.mkdir(path)
-            print('Pasta Criada!')
-        else:
-            print(colors.red +'Pasta já existe'+ colors.reset)
-        #
-    except OSError:
-        print("Não foi possível encontrar o caminho! Verifique!!")
-        raise
-    except Exception:
-        raise
-    # 
-    print(colors.blue + output.decode('latin-1') + colors.reset)
- 
+
+                
+
+    def download_audio(audio_url,path_audio ):  
+        path_audio = 'Audio_Download' 
+        command_audio = ["yt-dlp", "-x", "--audio-format", "mp3", audio_url, "-o", f'{path_audio}/%(title)s.%(ext)s']
+        output_audio = subprocess.check_output(command_audio) 
+        
+        try:
+            if not os.path.exists(path_audio):
+                os.mkdir(path_audio)
+                print('Pasta Criada!')
+            else:
+                print(colors.red +'Pasta já existe'+ colors.reset)
+            #
+        except OSError:
+            print("Não foi possível encontrar o caminho! Verifique!!")
+            raise
+        except Exception:
+            raise
+        # 
+        print(colors.blue + output_audio.decode('latin-1') + colors.reset)
 
             
-
-def download_audio(audio_url,path_audio ):  
-    path_audio = 'Audio_Download' 
-    command_audio = ["yt-dlp", "-x", "--audio-format", "mp3", audio_url, "-o", f'{path_audio}/%(title)s.%(ext)s']
-    output_audio = subprocess.check_output(command_audio) 
-     
-    try:
-        if not os.path.exists(path_audio):
-            os.mkdir(path_audio)
-            print('Pasta Criada!')
-        else:
-            print(colors.red +'Pasta já existe'+ colors.reset)
-        #
-    except OSError:
-        print("Não foi possível encontrar o caminho! Verifique!!")
-        raise
-    except Exception:
-        raise
-    # 
-    print(colors.blue + output_audio.decode('latin-1') + colors.reset)
-
+    def playlist_audio(playlist_url,path_audio_playlist ):
+        path_audio_playlist = 'Audios_Download' 
         
-def playlist_audio(playlist_url,path_audio_playlist ):
-    path_audio_playlist = 'Audios_Download' 
-    
-    command_playlist = f"yt-dlp --extract-audio --audio-format mp3\
-        '{playlist_url}' -o '{path_audio_playlist}/%(title)s'"
-    output_audio_playlist = subprocess.check_output(command_playlist, shell=True)
-    
-    try:
-        if not os.path.exists(path_audio_playlist):
-            os.mkdir(path_audio_playlist)
-            print('Pasta Criada!')
-        else:
-            print(colors.red +'Pasta já existe'+ colors.reset)
-        #
-    except OSError:
-        print("Não foi possível encontrar o caminho! Verifique!!")
-        raise
-    except Exception:
-        raise
-    # 
-    print(colors.blue + output_audio_playlist.decode('latin-1') + colors.reset)
+        command_playlist = f"yt-dlp --extract-audio --audio-format mp3\
+            '{playlist_url}' -o '{path_audio_playlist}/%(title)s'"
+        output_audio_playlist = subprocess.check_output(command_playlist, shell=True)
+        
+        try:
+            if not os.path.exists(path_audio_playlist):
+                os.mkdir(path_audio_playlist)
+                print('Pasta Criada!')
+            else:
+                print(colors.red +'Pasta já existe'+ colors.reset)
+            #
+        except OSError:
+            print("Não foi possível encontrar o caminho! Verifique!!")
+            raise
+        except Exception:
+            raise
+        # 
+        print(colors.blue + output_audio_playlist.decode('latin-1') + colors.reset)
 
            
   
@@ -85,36 +86,32 @@ def playlist_audio(playlist_url,path_audio_playlist ):
 
    
    
-def main():
-  
-    interation = input(colors.green +'Deseja baixar\
- [V]ideo  [A]udio  ou [Ap]Audio_playlist? :  '+ colors.reset).lower()
+    def main():
+    
+        interation = input(colors.green +'Deseja baixar [V]ideo  [A]udio  ou [Ap]Audio_playlist? :  '+ colors.reset).lower()
 
-    match interation:
-        case 'v':
-            movie_url = input(colors.green +'Informe o link do vídeo ou playlist: '+ colors.reset)
-            output = download_movie(movie_url, './Videos_Download')
+        match interation:
+            case 'v':
+                movie_url = input(colors.green +'Informe o link do vídeo ou playlist: '+ colors.reset)
+                output = download_movie(movie_url, './Videos_Download')
 
-        case 'a':         
-            audio_url = input(colors.green +'Informe o link do vídeo que você deseja obter o audio: '+ colors.reset)
-            output = download_audio(audio_url, './musica_Download')
-   
-        case 'ap':         
-            playlist_url = input(colors.green +'Informe o link da playlist que você deseja obter o áudio: '+ colors.reset)
-            output =  playlist_audio(playlist_url, './musicas_Download')
+            case 'a':         
+                audio_url = input(colors.green +'Informe o link do vídeo que você deseja obter o audio: '+ colors.reset)
+                output = download_audio(audio_url, './musica_Download')
+    
+            case 'ap':         
+                playlist_url = input(colors.green +'Informe o link da playlist que você deseja obter o áudio: '+ colors.reset)
+                output =  playlist_audio(playlist_url, './musicas_Download')
 
-        case _:
-            print(colors.red +'parâmetro inválido'+ colors.reset)
-            exit(1)     
-        
+            case _:
+                print(colors.red +'parâmetro inválido'+ colors.reset)
+                exit(1)     
+            
 
 
     
-print(f"""{colors.red}
-          
-
-
-
+    print(f"""{colors.red}
+            
 
  __      ____    _              _____                      _                 _           
  \ \    / /_/   | |            |  __ \                    | |               | |          
@@ -122,15 +119,25 @@ print(f"""{colors.red}
    \ \/ / | |/ _` |/ _ \/ _ \  | |  | |/ _ \ \ /\ / / '_ \| |/ _ \ / _` |/ _` |/ _ \ '__|
     \  /  | | (_| |  __/ (_) | | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| |  __/ |   
      \/   |_|\__,_|\___|\___/  |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|\___|_|   
-                                                                                         
-                                                                                         
+
+
+
+                                                                                            
+                                                                                            
 
 
 
 
-        
- {colors.reset}""")
+            
+    {colors.reset}""")
 
-if __name__ == '__main__':
-    main()   
-        
+    if __name__ == '__main__':
+        main()   
+    
+    Exit = input(colors.green +'Deseja sair? (y/n) '+ colors.reset).lower()
+    if Exit == 'y':
+        break   
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+            
